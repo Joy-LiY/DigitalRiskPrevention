ec82b7a0d72f46a0f73985c828b7b21dddb67359
taozi <1449434695@qq.com>
2023-04-03 09:03:09 +0800

    【提交说明】 修改页面返回地址值

--- src/main/java/com/example/digitalriskprevention/controller/DataAnalysisController.java
+++ src/main/java/com/example/digitalriskprevention/controller/DataAnalysisController.java
@@ -1,13 +1,8 @@
 package com.example.digitalriskprevention.controller;
 
-import cn.hutool.json.JSON;
-import cn.hutool.json.JSONArray;
 import com.example.digitalriskprevention.service.RequirementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -25,9 +20,9 @@ public class DataAnalysisController {
     @Autowired
     RequirementService requirementService;
 
-    @RequestMapping("/index")
+    @RequestMapping("index")
     public String index() {
-        return "/index";
+        return "index";
     }
 
     /**
